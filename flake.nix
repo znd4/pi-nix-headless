e2eb7fc8cb3d36ae7196ae0544c3b1a04cdc3f24
Zane Dufour <zane@znd4.dev>
2024-08-19 19:43:42 -0400

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,17 +1,22 @@
 {
   description = "Minimal NixOS installation media";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      exampleIso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-            environment.systemPackages = [ pkgs.neovim ];
-          })
-        ];
+  outputs =
+    { self, nixpkgs }:
+    {
+      nixosConfigurations = {
+        exampleIso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (
+              { pkgs, modulesPath, ... }:
+              {
+                imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+                environment.systemPackages = [ pkgs.neovim ];
+              }
+            )
+          ];
+        };
       };
     };
-  };
 }
